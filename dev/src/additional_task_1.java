b316d4da30f2a42e7b7439a0eb9cc8fd0a1dafa3
Kot19 <kolya.kovalec@ukr.net>
2016-08-17 20:56:18 +0300

    Initial commit

---
+++ dev/src/additional_task_1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Moshe Balanga on 17.08.2016.
+ */
+public class additional_task_1 {
+    public static void main(String[] args) {
+        System.out.println("Lesson-1");
+    }
+}
